d0f9f7beee05fbc2cd3e19482d7fda1c31998374
rbettN <bettoni.rafa@gmail.com>
2021-04-05 23:28:44 -0400

    Lesson257: Set up a Subscription to the store 'store.subscribe()' in redux-basics.js

--- redux-basics.js
+++ redux-basics.js
@@ -26,6 +26,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription
+/*The subscribe function takes an argument, which is a function that is executed
+whenever the state gets updated, i.e., whenever an action reaches the reducer*/
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState())
+})
+
 //Dispatching
 /*The dispatch function takes an argument (action) which is a JavaScript object which
 needs to have a type*/
@@ -33,5 +40,4 @@ store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
 
-//Subscription
 
