cb3fb9c1e3e68e6531cfc7dbb8f8cc7c773bf93d
rbettN <bettoni.rafa@gmail.com>
2021-04-12 22:38:17 -0400

    Lesson283-284: Added a middleware to the store in the index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 /*Only importing the store doesn't connect the store to React*/
 /*combineReducers allows to combine the reducers. It is a function which takes a JavaScript object, mapping the reducers
 to different slices of the state as input and merges everything into one state, one reducer*/
-import { createStore, combineReducers} from 'redux'; 
+/*applyMiddleware allows to add our own middleware to the stores*/ 
+import { createStore, combineReducers, applyMiddleware } from 'redux'; 
 /*Provider is a helper component that allows to inject the store into the react components*/
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
@@ -15,8 +16,20 @@ import resultReducer from './store/reducers/result';
 const rootReducers = combineReducers({
     ctr: counterReducer,
     res: resultReducer
-})
-const store = createStore(rootReducers);
+});
+
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducers, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
